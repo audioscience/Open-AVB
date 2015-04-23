efc53f719d8732fa872c1bb64eb6ffa9a005503c
stevengear <smgear@att.net>
2015-04-23 09:39:04 -0400

    Revert "avb: Removed "ifdef OLD_GPT" section."
    This reverts commit 1bdc30820268a52e46b843a04dc2d98c420b4395.

--- daemons/gptp/common/avbts_clock.hpp
+++ daemons/gptp/common/avbts_clock.hpp
@@ -245,6 +245,7 @@ public:
 
 	void setSharedAsCapable(bool asCapable);
 	bool checkPriority1Update(uint32_t *newPriority);
+#ifdef OLD_GPTP
 	void setMasterOffset
 	( int64_t master_local_offset, Timestamp local_time,
 		FrequencyRatio master_local_freq_offset, int64_t local_system_offset,
@@ -252,7 +253,12 @@ public:
 		unsigned sync_count, unsigned pdelay_count, PortState port_state , long long pTime,
 		Timestamp raw_system_time, Timestamp raw_device_time,
 		long long qp_prior_time,  long long qp_after_time, long long ll_retry_count );
-	  FrequencyRatio master_local_freq_offset,
+#else
+
+	void setMasterOffset
+	( struct masterToLocal master_to_local, struct localToSystem local_to_system,
+	  unsigned sync_count, unsigned pdelay_count, PortState port_state );
+#endif
 	
 	ClockIdentity getClockIdentity() {
 		return clock_identity;
